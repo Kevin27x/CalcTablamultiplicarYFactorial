a6b48361216ef3a3eb5c0da5138cb61669f534cd
KevinC <Kevin.collao1@gmail.com>
2022-01-20 20:23:55 -0300

    Arreglos esteticos, y comentarios js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,29 @@
 // Funcion arrow de multiplicacion
 let multi = (numero) => {
     for (i = 1; i <= numero; i++){
-        while (i <= numero){
-            let multip = i * numero;
-            let showResult = i + " x " + numero + " = " + multip;
-            console.log(showResult);
-            document.getElementById("results").innerHTML += "<p>" + showResult + "</p>";
-            // incrementa el iterador
-            i++
-          
-           
-        }
+    
+        let multip = i * numero;
+        let showResult = i + " x " + numero + " = " + multip;
+        //resultado en consola
+        console.log(showResult);
+        //resultado en HTML, "results", entre etiquetas <p> y acumulativos +=
+        document.getElementById("results").innerHTML += "<p>" + showResult + "</p>";
+
     }
-}    
+}
+    
 // Funcion arrow de factorial
 let factorial = (numero) => {
+    //Acumula el factorial del ciclo anterior, comienza en 1
     lastResult = 1;
     for (i = 1; i <= numero; i++){
         
-        //Calcula el factorail, multiplica i por el ultimo factorial
+        //Calcula el factorial, multiplica i por el último factorial
         let result = lastResult * i;
         let showResult = "Factorial de " + i +" es : " + result;
+        //resultado en consola
         console.log(showResult);
+        //resultado en HTML, "results", entre <p> y acumulativos 
         document.getElementById("results").innerHTML += "<p>" + showResult + "</p>";
 
         //Almacena ultimo factorial para el siguiente ciclo
@@ -29,21 +31,20 @@ let factorial = (numero) => {
        
     }
 }
-//Solicitar entradas
-//const results = document.getElementById("results")
+//Se activa con el botón, entra número y salen resultados de f. multi y factorial
 var app = () => {
     let numero = prompt("Ingrese un número del 1 al 20");
+    // Si numero está entre 1 y 20 => ejecutar
     if (numero > 0 && numero <= 20){
-        // Si numero está entre 0 y 20 => ejecutar
-
-        //Limpiar elemento "results", del resultado anterior
+        
+        //Limpiar elemento id "results", del resultado anterior
         document.getElementById("results").innerHTML = "";
         multi(numero);
         factorial(numero);
     }
         
     else {
-        alert("Numero fuera de rango (debe estar entre el 1 y 20)")
+        alert("Numero fuera de rango (debe estar entre el 1 y 20)");
     }
 }
 
