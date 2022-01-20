1704e9020c4e03abe30f1dd28d36e59281fb9744
KevinC <Kevin.collao1@gmail.com>
2022-01-20 19:59:31 -0300

    Proyecto con JS, ESTILOS Y HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,13 @@ let multi = (numero) => {
     for (i = 1; i <= numero; i++){
         while (i <= numero){
             let multip = i * numero;
-            console.log(i + " x " + numero + " = " + multip);
+            let showResult = i + " x " + numero + " = " + multip;
+            console.log(showResult);
+            document.getElementById("results").innerHTML += "<p>" + showResult + "</p>";
+            // incrementa el iterador
             i++
+          
+           
         }
     }
 }    
@@ -15,22 +20,31 @@ let factorial = (numero) => {
         
         //Calcula el factorail, multiplica i por el ultimo factorial
         let result = lastResult * i;
-        console.log("Factorial de " + i +" es : " + result);
+        let showResult = "Factorial de " + i +" es : " + result;
+        console.log(showResult);
+        document.getElementById("results").innerHTML += "<p>" + showResult + "</p>";
 
         //Almacena ultimo factorial para el siguiente ciclo
         lastResult = result;
-        
+       
     }
 }
+//Solicitar entradas
+//const results = document.getElementById("results")
+var app = () => {
+    let numero = prompt("Ingrese un número del 1 al 20");
+    if (numero > 0 && numero <= 20){
+        // Si numero está entre 0 y 20 => ejecutar
 
-var numero = prompt("Ingrese un número del 1 al 20");
-if (numero > 0 && numero <= 20){
-    multi(numero);
-    factorial(numero);
-}
-    
-else {
-    alert("Numero fuera de rango (debe estar entre el 1 y 20)")
+        //Limpiar elemento "results", del resultado anterior
+        document.getElementById("results").innerHTML = "";
+        multi(numero);
+        factorial(numero);
+    }
+        
+    else {
+        alert("Numero fuera de rango (debe estar entre el 1 y 20)")
+    }
 }
 
 
